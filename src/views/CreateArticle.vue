3eeba22f6569009f487ad9cbda4cbd8b50f1aaa3
AndreiDetenkov <a.detenkov@gmail.com>
2020-11-29 13:59:02 +0600

    creating Article Form component

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    Create article
+    <mcv-article-form
+      :initial-values="initialValues"
+      :errors="validationErrors"
+      :is-submitting="isSubmitting"
+      @article-submit="onSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import McvArticleForm from '@/components/ArticleForm'
+
+export default {
+  name: 'McvCreateArticle',
+  components: {McvArticleForm},
+  data() {
+    return {
+      initialValues: {
+        title: '',
+        descrition: '',
+        body: '',
+        tagList: []
+      },
+      validationErrors: null,
+      isSubmitting: false
+    }
+  },
+  methods: {
+    onSubmit(data) {
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
